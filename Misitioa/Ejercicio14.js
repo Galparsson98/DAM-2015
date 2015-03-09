487e29cc2946aa8b9ed186a05781e20a9cfb1934
unknown <FPE@965PDS3-004.aeg8.edu>
2015-03-09 14:24:44 +0100

    Javascript

---
+++ Misitioa/Ejercicio14.js
@@ -0,0 +1,6 @@
+var numero = document.getElementsByTagName("a");
+    console.log (numero.length);
+var dir =numero.item (numero.length -2);
+    console.log (dir);
+var nm = document.getElementById("http://prueba");
+        console.log (nm);

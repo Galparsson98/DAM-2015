01cc2bf75a2a059f6c72cc9a860bb1fa40ef98d4
unknown <FPE@965PDS3-004.aeg8.edu>
2015-03-09 14:38:01 +0100

    ejercicios java

--- Misitioa/Ejercicio14.js
+++ Misitioa/Ejercicio14.js
@@ -2,5 +2,10 @@ var numero = document.getElementsByTagName("a");
     console.log (numero.length);
 var dir =numero.item (numero.length -2);
     console.log (dir);
-var nm = document.getElementById("http://prueba");
-        console.log (nm);
+var contar=0
+ for (var i=0; i<numero.length; i++){
+        if (numero[i].href = "http://prueba"){
+            contar++;}
+     }
+ console.log (contar);
+

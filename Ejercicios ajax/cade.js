c7f714629bb00a13b2d449c0dcf2b9721547bd0e
unknown <FPE@965PDS3-004.aeg8.edu>
2015-03-10 13:44:12 +0100

    Ejercicios ajax2

---
+++ Ejercicios ajax/cade.js
@@ -0,0 +1,14 @@
+var dirpagina= document.location.href;
+    window.onload= dirpagina;
+var cajaurl= document.getElementById("recurso");
+cajaurl.value= dirpagina ;
+enviar.onclick(bidali); 
+function bidali (){
+     var berria= window.XMLHttpRequest(cajaurl,getAllResponseHeaders,false);
+    }
+var edukiak = bidali;
+var READY_STATE_UNINITIALIZED=0; 
+var READY_STATE_LOADING=1; 
+var READY_STATE_LOADED=2;
+var READY_STATE_INTERACTIVE=3; 
+var READY_STATE_COMPLETE=4;

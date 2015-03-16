908740a0011072f6fc527e3f223e08d3d6a0fa8a
unknown <FPE@965PDS3-004.aeg8.edu>
2015-03-16 14:19:54 +0100

    Cambiios

--- html5/Ej04/Placeholder.js
+++ html5/Ej04/Placeholder.js
@@ -1,13 +1,6 @@
-Modernizr.load({
-  test: Modernizr.geolocation,
-  yep : 'geo.js',
-  nope: 'geo-polyfill.js'
-});
-Modernizr.load([
-  // Presentational polyfills
-  {
-    // Logical list of things we would normally need
-    test : Modernizr.fontface && Modernizr.canvas && Modernizr.cssgradients,
-    // Modernizr.load loads css and javascript by default
-    nope : ['presentational-polyfill.js', 'presentational.css']
-  },
+ window.onload = function (cargado){
+   console.log (cargado);
+   Modernizr.load({
+  test:Modernizr.input.Placeholder,
+     nope:"https://raw.githubusercontent.com/philipwalton/polyfill/master/dist/polyfill.min.js"})
+    }

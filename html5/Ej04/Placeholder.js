dfd56d3796ccee573ac257a1536f8d59d7f58409
Galparsson98 <mikgalpar@gmail.com>
2015-03-26 13:01:03 +0100

    Eliminaciones

--- html5/Ej04/Placeholder.js
+++
@@ -1,6 +0,0 @@
- window.onload = function (cargado){
-   console.log (cargado);
-   Modernizr.load({
-  test:Modernizr.input.Placeholder,
-     nope:"https://raw.githubusercontent.com/philipwalton/polyfill/master/dist/polyfill.min.js"})
-    }
